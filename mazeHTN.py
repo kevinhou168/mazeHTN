8cac88627671c725bfeaa45bc34e6df03eae71c1
kevinhou168 <kevinhou168@gmail.com>
2019-07-17 16:51:18 -0500

    Begin integration with depth-first maze algorithm

--- mazeHTN.py
+++ mazeHTN.py
@@ -1,3 +1,4 @@
+import maze_layout
 from __future__ import print_function
 from pyhop import *
 
@@ -10,21 +11,11 @@ print('')
 print_operators()
 
 state1 = State('state1')
-state1.x = {'me':5}
-state1.y = {'me':7}
-state1.xpath = {'me':[5]}
+state1.x = {'me':0}
+state1.y = {'me':0}
+state1.xpath = {'me':[5]} # Record of traversed
 state1.ypath = {'me':[7]}
 state1.count = {'me':1}
-
-state1.maze =  [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                [1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
-                [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-                [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-                [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-                [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-                [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-
+state1.maze = maze_layout.maze.make_maze()
 
 pyhop(state1, [('FindGoal', 'me')], verbose=3)
