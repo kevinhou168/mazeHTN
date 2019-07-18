685151f3bb2978c228ab25dd76655b7714a4a8cd
kevinhou168 <kevinhou168@gmail.com>
2019-07-18 14:45:28 -0500

    Prepare integration with maze code

--- mazeHTN.py
+++ mazeHTN.py
@@ -13,9 +13,11 @@ print_operators()
 state1 = State('state1')
 state1.x = {'me':0}
 state1.y = {'me':0}
-state1.xpath = {'me':[5]} # Record of traversed
-state1.ypath = {'me':[7]}
+state1.xpath = {'me':[0]}  # Record of traversed paths
+state1.ypath = {'me':[0]}
 state1.count = {'me':1}
+state1.goal_x = {'me':state1.maze.nx}
+state1.goal_y = {'me':state1.maze.ny}
 state1.maze = maze_layout.maze.make_maze()
 
 pyhop(state1, [('FindGoal', 'me')], verbose=3)
